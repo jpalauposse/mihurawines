5918ed01a19d46af492ef6c76839764bb465c372
Javier Palau <javopa@yahoo.com.ar>
2020-08-18 21:26:36 +0200

    Se quitan los indicadores del carrusel

--- resources/views/indexEN.blade.php
+++ resources/views/indexEN.blade.php
@@ -4,11 +4,6 @@
     <?php include 'headerEN.php' ?>
     <div class="">
       <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img src="/pictures/hojas_bn.jpg" class="d-block w-100" alt="viñedo">
